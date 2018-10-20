05cc9c32c60908964f5d8f57c6b9c94da7c9207c
Alexis Ahmed <alexis@hsploit.com>
2018-10-20 06:47:03 +0300

    Update loader.js

--- app/assets/loader.js
+++ app/assets/loader.js
@@ -1,5 +1,5 @@
 require("./stylesheets/app.scss");
-require("file-loader?name=favicon-numex.png!./favicon-numex.ico");
+require("file-loader?name=favicon-numex.png!./favicon-numex.png");
 require("babel-polyfill");
 require("whatwg-fetch");
 require("indexeddbshim");
