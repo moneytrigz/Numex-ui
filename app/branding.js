68920dcb650b61fb7f9205d7250b04fcb2aed439
MoneyTrigz <moneytrigz@users.noreply.github.com>
2018-10-21 16:15:51 +0200

    Update branding.js

--- app/branding.js
+++ app/branding.js
@@ -77,7 +77,7 @@ export function getUnits(chainId = "4018d784") {
  */
 
 export function getMyMarketsBases() {
-    return ["BTC", "ETH", "USD", "CNY"];
+    return ["BTC", "ETH", "BTS", "USD", "CNY"];
 }
 
 /**
